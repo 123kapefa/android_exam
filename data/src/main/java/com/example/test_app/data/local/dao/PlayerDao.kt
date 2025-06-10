e27b738f5b7d547fd8119b1f385482ba954f47d9
artur <105075817+123kapefa@users.noreply.github.com>
2025-06-10 19:17:49 +0300

    Добавлены PlayerEntity.kt и PlayerDao.kt

---
+++ data/src/main/java/com/example/test_app/data/local/dao/PlayerDao.kt
@@ -0,0 +1,16 @@
+package com.example.test_app.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.test_app.data.local.entity.PlayerEntity
+
+@Dao
+interface PlayerDao {
+    @Query("SELECT * FROM player LIMIT 1")
+    suspend fun getCurrent(): PlayerEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(player: PlayerEntity)
+}
