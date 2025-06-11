53a6313f868c2654e3b7f52a1d9360d704bf8df0
artur <105075817+123kapefa@users.noreply.github.com>
2025-06-11 21:06:53 +0300

    update LoginFragment.kt

--- presentation/src/main/java/com/example/test_app/presentation/fragment/LoginFragment.kt
+++ presentation/src/main/java/com/example/test_app/presentation/fragment/LoginFragment.kt
@@ -1,6 +1,5 @@
 package com.example.test_app.presentation.fragment
 
-import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -10,7 +9,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.test_app.presentation.R
-import com.example.test_app.presentation.viewmodel.AchievementListViewModel
 import com.example.test_app.presentation.viewmodel.LoginViewModel
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -28,12 +26,8 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         button.setOnClickListener {
             val steamId = editText.text.toString().trim()
             if (steamId.isNotEmpty()) {
-                // запускаем корутину во вью-цикле, чтобы не блокировать UI
                 viewLifecycleOwner.lifecycleScope.launch {
-                    // 1. сначала тянем и сохраняем профиль игрока
-                    // 2. затем — кэш достижений
                     viewModel.login(steamId)
-                    // когда оба запроса завершились — переходим к списку
                     findNavController().navigate(R.id.action_loginFragment_to_achievementListFragment)
                 }
             } else {
